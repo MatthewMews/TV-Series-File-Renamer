fdac03c4582d35abaaff9def2fa8a9eade068fc4
matthew <matthew.mews@outlook.com>
2019-02-20 15:54:20 +0000

    Added more error handling in the event a file already exists or if there's no input from the user.

--- Main.cs
+++ Main.cs
@@ -4,117 +4,137 @@ using System.Linq;
 
 namespace TVSeriesFileRenamer
 {
-	public class FileRenamer
-	{
-		public static string directoryOfFiles;
-		public static string[] currentFileNames; // Files are not being retrieved just yet in order to prevent the program from stalling if the folder doesn't exist.
+    public class FileRenamer
+    {
+        public static string directoryOfFiles;
+        public static string[] currentFileNames; // Files are not being retrieved just yet in order to prevent the program from stalling if the folder doesn't exist.
 
-		public static void Main(string[] args)
-		{
-			Console.Title = "TV Series File Renamer";
+        public static void Main(string[] args)
+        {
+            Console.Title = "TV Series File Renamer";
 
-			AskForDirectoryPath ();
+            AskForDirectoryPath();
 
-			CheckDirectoryAndFilesExists (directoryOfFiles);
+            CheckDirectoryAndFilesExists(directoryOfFiles);
 
-			RenameVideoFolder (ref currentFileNames, ref directoryOfFiles);
+            RenameVideoFolder(ref currentFileNames, ref directoryOfFiles);
 
-            RenameFiles.ScanAndRenameFiles (currentFileNames);
+            RenameFiles.ScanAndRenameFiles(currentFileNames);
 
-			Console.WriteLine ("\nFile renaming complete.");
-		}
+            Console.WriteLine("\nFile renaming complete.");
+        }
 
-		private static void AskForDirectoryPath()
-		{
-			Console.WriteLine ("Please enter the folder path the videos are located in.\n");
-			string userInputFilePath = Console.ReadLine ();
+        private static void AskForDirectoryPath()
+        {
+            Console.WriteLine("Please enter the folder path the videos are located in.\n");
+            string userInputFilePath = Console.ReadLine();
 
-			if (userInputFilePath.ToLower() == "exit")
-			{
-				Environment.Exit (1);
-			}
+            if (userInputFilePath.ToLower() == "exit")
+            {
+                Environment.Exit(1);
+            }
 
-			directoryOfFiles = userInputFilePath;
+            directoryOfFiles = userInputFilePath;
 
-			if (!Directory.Exists (userInputFilePath)) {
-				CheckDirectoryAndFilesExists(directoryOfFiles);
-			}
-		}
+            if (!Directory.Exists(userInputFilePath))
+            {
+                CheckDirectoryAndFilesExists(directoryOfFiles);
+            }
+        }
 
-		private static void CheckDirectoryAndFilesExists(string directoryOfFiles)
-		{
-			if (Directory.Exists (directoryOfFiles)) {
-				Console.WriteLine ("\nVideo Directory Found!\n");
+        private static void CheckDirectoryAndFilesExists(string directoryOfFiles)
+        {
+            if (Directory.Exists(directoryOfFiles))
+            {
+                Console.WriteLine("\nVideo Directory Found!\n");
 
-				currentFileNames = Directory.GetFiles (directoryOfFiles) // We can now go get those files. OrderBy organises the videos by name.
-					.Select(v => Path.GetFileName(v))
-					.Where (v => v.EndsWith (".mp4") || v.EndsWith (".avi") ||
-						v.EndsWith (".mkv") || v.EndsWith (".wmv")).ToArray();
+                currentFileNames = Directory.GetFiles(directoryOfFiles) // We can now go get those files. OrderBy organises the videos by name.
+                    .Select(v => Path.GetFileName(v))
+                    .Where(v => v.EndsWith(".mp4") || v.EndsWith(".avi") ||
+                       v.EndsWith(".mkv") || v.EndsWith(".wmv")).ToArray();
 
-				if(currentFileNames.Count() > 0)
-				{
-					Console.WriteLine ($"{currentFileNames.Count()} Video(s) found!\n");
+                if (currentFileNames.Count() > 0)
+                {
+                    Console.WriteLine($"{currentFileNames.Count()} Video(s) found!\n");
 
-				} else {
-					Console.WriteLine ("Error: No video(s) were found.\n");
-					Environment.Exit (2);
-				}
+                }
+                else
+                {
+                    Console.WriteLine("Error: No video(s) were found.\n");
+                    Environment.Exit(2);
+                }
 
-			} else {
-				Console.WriteLine ("\nError: Video Directory not found. Please try again.\n");
-				AskForDirectoryPath ();
-			}
-		}
+            }
+            else
+            {
+                Console.WriteLine("\nError: Video Directory not found. Please try again.\n");
+                AskForDirectoryPath();
+            }
+        }
 
-		private static void RenameVideoFolder(ref string[] currentFileNames, ref string directoryOfFiles)
-		{
-			Console.WriteLine ($"Do you wish to rename the directory {Path.GetFileName(directoryOfFiles)}? y / n");
-			string renameFolderUserInput = Console.ReadLine ();
+        private static void RenameVideoFolder(ref string[] currentFileNames, ref string directoryOfFiles)
+        {
+            Console.WriteLine($"Do you wish to rename the directory {Path.GetFileName(directoryOfFiles)}? y / n");
+            string renameFolderUserInput = Console.ReadLine();
 
-			if (renameFolderUserInput.ToLower() == "y" || renameFolderUserInput == "yes")
-			{
-				string pathWithoutDirectoryName = Directory.GetParent(directoryOfFiles).FullName + "/";
+            if (renameFolderUserInput.ToLower() == "y" || renameFolderUserInput == "yes")
+            {
+                string pathWithoutDirectoryName = Directory.GetParent(directoryOfFiles).FullName + "/";
 
-				Console.WriteLine ("\nPlease enter the folders new name.\n");
-				string userInputDirectoryNewName = Console.ReadLine ();
+                Console.WriteLine("\nPlease enter the folders new name.\n");
+                string userInputDirectoryNewName = Console.ReadLine();
 
-				if (!Directory.Exists (pathWithoutDirectoryName + userInputDirectoryNewName)) {
+                if (!Directory.Exists(pathWithoutDirectoryName + userInputDirectoryNewName))
+                {
 
-					Directory.Move (directoryOfFiles, pathWithoutDirectoryName + userInputDirectoryNewName);
-				} else {
-					Console.WriteLine("\nError: Unable to rename directory as there is already a directory with the same name.\n");
-					RenameVideoFolder (ref currentFileNames, ref directoryOfFiles);
+                    Directory.Move(directoryOfFiles, pathWithoutDirectoryName + userInputDirectoryNewName);
+                }
+                else
+                {
+                    Console.WriteLine("\nError: Unable to rename directory as there is already a directory with the same name.\n");
+                    RenameVideoFolder(ref currentFileNames, ref directoryOfFiles);
                     return;
-				}
+                }
 
-				directoryOfFiles = pathWithoutDirectoryName + userInputDirectoryNewName;
+                directoryOfFiles = pathWithoutDirectoryName + userInputDirectoryNewName;
 
-				if (Directory.Exists(pathWithoutDirectoryName + userInputDirectoryNewName))
-				{
-					Console.WriteLine ($"\nThe directory has been successfully renamed to {userInputDirectoryNewName}!");
-				} else {
+                if (Directory.Exists(pathWithoutDirectoryName + userInputDirectoryNewName))
+                {
+                    Console.WriteLine($"\nThe directory has been successfully renamed to {userInputDirectoryNewName}!");
+                }
+                else
+                {
                     Console.WriteLine("Error: Unable to rename directory.");
-				}
-			}
-		}
+                }
+            }
+        }
 
-		public static void RenameFile(string filename, string seriesId, string episodeId, string fileFormat)
-		{
-			Console.WriteLine ($"\nWhat is the name of {seriesId} {episodeId}?\n");
-			string episodeName = Console.ReadLine ();
+        public static void RenameFile(string filename, string seriesId, string episodeId, string fileFormat)
+        {
+            Console.WriteLine($"\nWhat is the name of {seriesId} {episodeId}?\n");
+            string episodeName = Console.ReadLine();
 
-			File.Move (directoryOfFiles + "/" + filename, directoryOfFiles + "/" + seriesId + " " + episodeId + " - " + episodeName + fileFormat);
+            if (File.Exists(directoryOfFiles + "/" + seriesId + " " + episodeId + " - " + episodeName + fileFormat))
+            {
+                Console.WriteLine($"\nThe file {seriesId + episodeId + episodeName + fileFormat} already exists.");
+                RenameFile(filename, seriesId, episodeId, fileFormat);
+                return;
+            }
 
-			if (File.Exists(directoryOfFiles + "/" + seriesId + " " + episodeId + " - " + episodeName + fileFormat))
-			{
-				Console.ForegroundColor = ConsoleColor.Green;
-				Console.WriteLine ("\nFile has been renamed succesfully!");
-			} else {
-				Console.ForegroundColor = ConsoleColor.Red;
-				Console.WriteLine ("\nError: Unable to rename file.");
-			}
+            File.Move(directoryOfFiles + "/" + filename, directoryOfFiles + "/" + seriesId + " " + episodeId + " - " + episodeName + fileFormat);
 
-			Console.ResetColor ();
-		}
-	}
+            if (File.Exists(directoryOfFiles + "/" + seriesId + " " + episodeId + " - " + episodeName + fileFormat))
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("\nFile has been renamed succesfully!");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\nError: Unable to rename file.");
+            }
+
+            Console.ResetColor();
+        }
+    }
 }
--- ScanForFiles.cs
+++ ScanForFiles.cs
@@ -4,87 +4,88 @@ using TVSeriesFileRenamer;
 
 namespace TVSeriesFileRenamer
 {
-	public class RenameFiles
-	{
-		public static void ScanAndRenameFiles(string[] currentFileNames)
-		{
-		    for (int i = 0; i <= currentFileNames.Length - 1; i++)
-		    {
-			string seriesId = String.Empty;
-			string episodeId = String.Empty;
-			string fileFormat = Path.GetExtension(currentFileNames[i]);
+    public class RenameFiles
+    {
+        public static void ScanAndRenameFiles(string[] currentFileNames)
+        {
+            for (int i = 0; i <= currentFileNames.Length - 1; i++)
+            {
+                string seriesId = String.Empty;
+                string episodeId = String.Empty;
+                string fileFormat = Path.GetExtension(currentFileNames[i]);
 
-			bool seriesFound = false;
-			bool episodeFound = false;
+                bool seriesFound = false;
+                bool episodeFound = false;
 
-			for (int j = 1; j <= 25; j++) // Detects up to 25 series.
-			{
-			    if ((j < 10) && (currentFileNames[i].ToLower().Contains("s0" + j) || currentFileNames[i].ToLower().Contains("series 0" + j) || (currentFileNames[i].ToLower().Contains("season 0" + j))))
-			    {
-				seriesId = "S0" + j;
-				seriesFound = true;
-				break;
-			    } else if ((j >= 10) && (currentFileNames[i].ToLower().Contains("s" + j) || currentFileNames[i].ToLower().Contains("series " + j) || (currentFileNames[i].ToLower().Contains("season " + j)))) 
-			    {
-				seriesId = "S" + j;
-				seriesFound = true;
-				break;
-			    }
-			}
+                for (int j = 1; j <= 25; j++) // Detects up to 25 series.
+                {
+                    if ((j < 10) && (currentFileNames[i].ToLower().Contains("s0" + j) || currentFileNames[i].ToLower().Contains("series 0" + j) || (currentFileNames[i].ToLower().Contains("season 0" + j))))
+                    {
+                        seriesId = "S0" + j;
+                        seriesFound = true;
+                        break;
+                    }
+                    else if ((j >= 10) && (currentFileNames[i].ToLower().Contains("s" + j) || currentFileNames[i].ToLower().Contains("series " + j) || (currentFileNames[i].ToLower().Contains("season " + j))))
+                    {
+                        seriesId = "S" + j;
+                        seriesFound = true;
+                        break;
+                    }
+                }
 
-			if(seriesFound == false) // in the event we cannot get the series number, ask the user to enter it manually.
-			{
-			    Console.ForegroundColor = ConsoleColor.Magenta;
-			    Console.WriteLine($"\nWarning - The program could not detect the show's season number for {currentFileNames[i]}.\nPlease enter the season number below:");
-			    Console.ResetColor();
+                if (seriesFound == false) // in the event we cannot get the series number, ask the user to enter it manually.
+                {
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    Console.WriteLine($"\nWarning - The program could not detect the show's season number for {currentFileNames[i]}.\nPlease enter the season number below:");
+                    Console.ResetColor();
 
-			    seriesId = Console.ReadLine();
+                    seriesId = Console.ReadLine();
 
-			    if (seriesId.ToUpper().ToCharArray()[0] != 'S')
-			    {
-				seriesId = seriesId.Insert(0, "S");
-			    }
-			}
+                    if (seriesId.ToUpper().ToCharArray()[0] != 'S')
+                    {
+                        seriesId = seriesId.Insert(0, "S");
+                    }
+                }
 
-			for (int k = 0; k <= 100; k++) // Detects up to 100 episodes.
-			{
-			    if ((k < 10) && (currentFileNames[i].ToLower().Contains("e0" + k) || currentFileNames[i].ToLower().Contains("episode 0" + k)))
-			    {
-				episodeId = "E0" + k;
-				episodeFound = true;
+                for (int k = 0; k <= 100; k++) // Detects up to 100 episodes.
+                {
+                    if ((k < 10) && (currentFileNames[i].ToLower().Contains("e0" + k) || currentFileNames[i].ToLower().Contains("episode 0" + k)))
+                    {
+                        episodeId = "E0" + k;
+                        episodeFound = true;
 
-				FileRenamer.RenameFile(currentFileNames[i], seriesId, episodeId, fileFormat);
+                        FileRenamer.RenameFile(currentFileNames[i], seriesId, episodeId, fileFormat);
 
-				break;
-			    }
-			    else if ((k >= 10) && (currentFileNames[i].ToLower().Contains("e" + k) || currentFileNames[i].ToLower().Contains("episode " + k)))
-			    {
-				episodeId = "E" + k;
-				episodeFound = true;
+                        break;
+                    }
+                    else if ((k >= 10) && (currentFileNames[i].ToLower().Contains("e" + k) || currentFileNames[i].ToLower().Contains("episode " + k)))
+                    {
+                        episodeId = "E" + k;
+                        episodeFound = true;
 
-				FileRenamer.RenameFile(currentFileNames[i], seriesId, episodeId, fileFormat);
+                        FileRenamer.RenameFile(currentFileNames[i], seriesId, episodeId, fileFormat);
 
-				break;
-			    }
-			}
+                        break;
+                    }
+                }
 
-			if (episodeFound == false) // in the event we cannot get the episode number, ask the user to enter it manually.
-			{
-			    Console.ForegroundColor = ConsoleColor.Magenta;
-			    Console.WriteLine($"\nWarning - The program could not detect the show's episode number for {FileRenamer.currentFileNames[i]}.\nPlease enter the season number below:");
-			    Console.ResetColor();
+                if (episodeFound == false) // in the event we cannot get the episode number, ask the user to enter it manually.
+                {
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    Console.WriteLine($"\nWarning - The program could not detect the show's episode number for {FileRenamer.currentFileNames[i]}.\nPlease enter the season number below:");
+                    Console.ResetColor();
 
-			    episodeId = Console.ReadLine();
+                    episodeId = Console.ReadLine();
 
-			    if (episodeId.ToUpper().ToCharArray()[0] != 'E')
-			    {
-				episodeId = episodeId.Insert(0, "E");
-			    }
+                    if (episodeId.ToUpper().ToCharArray()[0] != 'E')
+                    {
+                        episodeId = episodeId.Insert(0, "E");
+                    }
 
-			    FileRenamer.RenameFile(currentFileNames[i], seriesId, episodeId, fileFormat);
-			}
-		    }
-		}
-	}
+                    FileRenamer.RenameFile(currentFileNames[i], seriesId, episodeId, fileFormat);
+                }
+            }
+        }
+    }
 }
 
